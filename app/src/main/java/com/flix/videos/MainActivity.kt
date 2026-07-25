19cc406fd241c13bea24eb01239a8b80358517eb
Administrator <santhoshcandy44@gmail.com>
2026-07-25 13:20:01 +0530

    bug fixes

--- app/src/main/java/com/flix/videos/MainActivity.kt
+++ app/src/main/java/com/flix/videos/MainActivity.kt
@@ -4,15 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.retain.retain
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.flix.videos.ui.app.MainVideosScreen
 import com.flix.videos.ui.app.PermissionWrapper
@@ -28,20 +20,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
-
         setContent {
-            val safeDrawing = WindowInsets.safeDrawing
-            val configuration = LocalConfiguration.current
-            var initialSafeInsets by retain {
-                mutableStateOf(safeDrawing)
-            }
-
-            LaunchedEffect(configuration) {
-                initialSafeInsets = safeDrawing
-            }
-
             AppTheme {
-                SafeDrawing(windowInsets = initialSafeInsets) {
+                SafeDrawing {
                     val viewModel: ReadMediaVideosViewModel = koinViewModel()
                     PermissionWrapper(onPermissionGranted = {
                         viewModel.fetchVideoInfos()
